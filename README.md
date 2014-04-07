97b927f667668a8aa331d80d42682fed1a9226a5
ConstantA <panurgomatic@gmail.com>
2014-04-07 20:11:05 +0400

    Initial commit
    Signed-off-by: ConstantA <panurgomatic@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 jquery-element-truncator
 ========================
 
-Simple jQuery plugin to truncate element 'lists' and then expand
+Simple jQuery plugin to truncate element 'lists' and then expand\collapse them with animation. Supports CSS3 transitions, adds custom events.
+
+Example: http://jsfiddle.net/ConstantA/LA9Zr/
+
+Init options:
+
++ `child_selector` - String. jQuery selector string to get elements to truncate. Default: `false` (uses .`children()`);
++ `truncate_to` - Integer. Number of elements to truncate to. Default: `6`;
++ `animation_duration` - Integer, ms. Animation speed in ms. Default: `500`;
++ `button_class` - String. Control button class. Default: `'collapse_button'`;
++ `button_element` - HTML tag. Control button tag. Default: `'<div/>'`;
++ `button_text_expand` - String. This text will be shown on button when plugin is collapsed. Default: `'Show all'`;
++ `button_text_collapse` - String. This text will be shown on button when plugin is expanded. Default: `'Collapse'`;
++ `control_class` - String. Control button wrapper class. Default: `'collapse_control'`;
++ `control_element` - HTML tag. Control button wrapper tag. Default: `'<div/>'`;
++ `wrapper_collapsed_class` - String. Wrapper class when collapsed; Default: `'collapsed'`;
++ `csstransitions` - Boolean. Use css transitions or not. Default: `false`;
+
+Events:
+
++ `beforeExpand` - triggered when plugin is collapsed and control button is pressed, before expand animation start.
++ `afterExpand` - triggered when expand animation is finished.
++ `beforeCollapse` - triggered when plugin is expanded and control button is pressed, before collapse animation start.
++ `afterCollapse` - triggered when collapse animation is finished.
+  
+Example usage:
+
+```javascript
+  $(function () {
+    $('.num_trim').truncateToElements({
+        truncate_to: 5,
+        csstransitions: true
+    });
+    $('.num_trim').on('beforeExpand', function () {
+        console.log('Triggered beforeExpand event');
+    });
+    $('.num_trim').on('afterExpand', function () {
+        console.log('Triggered afterExpand event');
+    });
+    $('.num_trim').on('beforeCollapse', function () {
+        console.log('Triggered beforeCollapse event');
+    });
+    $('.num_trim').on('afterCollapse', function () {
+        console.log('Triggered afterCollapse event');
+    });
+  });
+```
---
+++ element-truncator.js
@@ -0,0 +1,144 @@
+$.fn.extend({
+    truncateToElements: function (options) {
+        var $wrapper = this,
+            settings = $.extend({
+                child_selector: false,
+                truncate_to: 6,
+                animation_duration: 500,
+                button_class: 'collapse_button',
+                button_element: '<div/>',
+                button_text_expand: 'Show all',
+                button_text_collapse: 'Collapse',
+                control_class: 'collapse_control',
+                control_element: '<div/>',
+                wrapper_collapsed_class: 'collapsed',
+                csstransitions: false
+            }, options),
+            $children = !settings.child_selector ? $wrapper.children() : $wrapper.find(settings.child_selector);
+        if (settings.truncate_to < 1 || $children.length <= settings.truncate_to) {
+            return;
+        }
+        var getElementBottom = function ($element) {
+            return $element.position().top + $element.outerHeight(true) - parseInt($element.css('marginTop'), 10);
+        };
+        var $last_element = $($children[settings.truncate_to - 1]);
+        $wrapper.css({
+            height: getElementBottom($last_element)
+        }).data('collapsed', true).addClass(settings.wrapper_collapsed_class);
+        var $children_hide_slice = $children.slice(settings.truncate_to);
+        $children_hide_slice.hide();
+        var $collapse_button = $(settings.button_element)
+            .addClass(settings.button_class)
+            .text(settings.button_text_expand);
+        $(settings.control_element)
+            .addClass(settings.control_class)
+            .insertAfter(this)
+            .append($collapse_button);
+        if (settings.csstransitions) {
+            var children_display = $children.css('display');
+            var wrapperCollapse = function () {
+                $children_hide_slice.css({
+                    opacity: 0
+                });
+                $wrapper.css({
+                    height: $wrapper.height()
+                });
+                $wrapper.css({
+                    height: getElementBottom($last_element)
+                });
+                return;
+            };
+            var wrapperExpand = function () {
+                $wrapper.css({
+                    height: $wrapper.height()
+                });
+                $children_hide_slice.css({
+                    display: children_display
+                });
+                $wrapper.css({
+                    height: getElementBottom($children.last())
+                });
+                $children_hide_slice.css({
+                    opacity: 1
+                });
+            };
+            var afterAnimation = function (e) {
+                if (event.target != this) {
+                    return;
+                }
+                switch ($wrapper.data('collapsed')) {
+                    case (true):
+                        $children_hide_slice.css({
+                            display: 'none'
+                        });
+                        $wrapper.css({
+                            height: 'auto'
+                        });
+                        $wrapper.trigger('afterCollapse');
+                        return;
+                    case (false):
+                        $wrapper.css({
+                            height: 'auto'
+                        });
+                        $wrapper.trigger('afterExpand');
+                        return;
+                }
+            };
+            $wrapper.css({
+                'transition': 'height ' + settings.animation_duration + 'ms'
+            });
+            $children_hide_slice.css({
+                opacity: 0
+            });
+            $children.css({
+                'transition': 'opacity ' + settings.animation_duration + 'ms'
+            });
+            $wrapper.on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', afterAnimation);
+        } else {
+            var wrapperCollapse = function () {
+                $wrapper.animate({
+                    height: getElementBottom($last_element)
+                }, settings.animation_duration, afterAnimation);
+                $children_hide_slice.fadeOut(settings.animation_duration * 0.9);
+            };
+            var wrapperExpand = function () {
+                $children.fadeIn(settings.animation_duration * 0.9);
+                $wrapper.animate({
+                    height: getElementBottom($children.last())
+                }, settings.animation_duration, afterAnimation);
+            };
+            var afterAnimation = function () {
+                switch ($wrapper.data('collapsed')) {
+                    case (true):
+                        $wrapper.css({
+                            height: 'auto'
+                        });
+                        $wrapper.trigger('afterCollapse');
+                        return;
+                    case (false):
+                        $wrapper.css({
+                            height: 'auto'
+                        });
+                        $wrapper.trigger('afterExpand');
+                        return;
+                }
+            };
+        }
+        $collapse_button.on('click', function () {
+            switch ($wrapper.data('collapsed')) {
+                case (true):
+                    $wrapper.trigger('beforeExpand');
+                    $wrapper.data('collapsed', false).removeClass(settings.wrapper_collapsed_class);
+                    $(this).text(settings.button_text_collapse);
+                    wrapperExpand();
+                    return;
+                case (false):
+                    $wrapper.trigger('beforeCollapse');
+                    $wrapper.data('collapsed', true).addClass(settings.wrapper_collapsed_class);
+                    $(this).text(settings.button_text_expand);
+                    wrapperCollapse();
+                    return;
+            }
+        });
+    }
+});
